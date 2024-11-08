eea8eae285cd8d186a9b3d2a4fdd94d8b39047d0
behradbina <behradbina@gmail.com>
2024-11-08 18:54:19 +0330

    syscall const define

--- syscall.c
+++ syscall.c
@@ -106,7 +106,7 @@ extern int sys_uptime(void);
 extern int sys_my_syscall(void);
 extern int sys_move_file(void);
 extern int sys_sort_syscalls(void);
-
+extern void sys_create_palindrome(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -133,7 +133,7 @@ static int (*syscalls[])(void) = {
 [SYS_my_syscall]   sys_my_syscall, 
 [SYS_move_file]   sys_move_file,
 [SYS_sort_syscalls] sys_sort_syscalls,
-
+[SYS_create_palindrome] sys_create_palindrome,
 };
 
 void
